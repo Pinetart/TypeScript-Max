9bc32aae544950d37791b77e02c5abfe8232ebcd
Zane Birkett <axelbirkett@gmail.com>
2022-03-24 09:29:30 -0400

    Working with Array & Object Types

---
+++ basics.ts
@@ -0,0 +1,35 @@
+// Primitives: number, strong, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+age = 12;
+
+let userName: string;
+userName = "Max";
+
+let isInstructor: boolean;
+isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ["sports", "cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
