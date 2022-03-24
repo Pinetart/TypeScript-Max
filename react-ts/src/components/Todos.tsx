3561b82f25478d5e5856aab35716a631c5efe5c6
Zane Birkett <axelbirkett@gmail.com>
2022-03-24 11:02:31 -0400

    Time to Practice: Exercise Time!

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Todo from "../models/todo";
+import TodoItem from "./TodosItem";
 
 const Todos: React.FC<{ items: Todo[] }> = ({ items }) => {
   return (
     <ul>
       {items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
