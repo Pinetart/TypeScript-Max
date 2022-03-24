d5b00739b7d20079450ab80c7a3cf777cff02895
Zane Birkett <axelbirkett@gmail.com>
2022-03-24 11:09:00 -0400

    Form Submissions In TypeScript Projects

---
+++ react-ts/src/components/NewTodo.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const NewTodo = () => {
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo text</label>
+      <input type="text" id="text" />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
